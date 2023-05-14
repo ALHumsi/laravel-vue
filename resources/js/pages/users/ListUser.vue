97a2536aab03281684f47c65252410c66aad17db
abdullah <abdullahalhomsi520@gmail.com>
2023-05-15 00:39:47 +0300

    add delete option

--- resources/js/pages/users/ListUser.vue
+++ resources/js/pages/users/ListUser.vue
@@ -10,6 +10,7 @@ const users = ref([]);
 const editing = ref(false);
 const formValues = ref();
 const form = ref(null);
+const userIdBeingDeleted = ref(null);
 
 const createUserSchema = yup.object({
     name: yup.string().required(),
@@ -80,6 +81,23 @@ const handleSubmit = (values, actions) => {
     }
 };
 
+// Delete User
+
+const confirmUserDeletion = (user) => {
+    userIdBeingDeleted.value = user.id;
+    $('#deleteUserModal').modal('show');
+};
+
+const deleteUser = () => {
+    axios.delete(`/api/users/${userIdBeingDeleted.value}`)
+    .then(() => {
+        $('#deleteUserModal').modal('hide');
+        users.value = users.value.filter(user => user.id !== userIdBeingDeleted.value)
+        toastr.success('User deleted successfully!');
+    });
+
+};
+
 onMounted(() => {
     getUsers();
 });
@@ -135,6 +153,8 @@ onMounted(() => {
                                 <td>Super Admin</td>
                                 <td>
                                     <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
+                                    <a href="#" @click.prevent="confirmUserDeletion(user)"><i
+                                            class="fa fa-trash text-danger ml-2"></i></a>
                                 </td>
                             </tr>
                         </tbody>
@@ -144,6 +164,7 @@ onMounted(() => {
         </div>
     </div>
 
+    <!-- Create and Update Modal -->
     <div class="modal fade" id="userFormModal" data-backdrop="static" tabindex="-1" role="dialog"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -187,6 +208,30 @@ onMounted(() => {
                         <button type="submit" class="btn btn-primary">Save</button>
                     </div>
                 </Form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Delete Modal -->
+    <div class="modal fade" id="deleteUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">
+                        <span>Delete User</span>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h5>Are you sure you want to delete this user ?</h5>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button @click.prevent="deleteUser" type="button" class="btn btn-primary">Delete User</button>
+                </div>
             </div>
         </div>
     </div>
